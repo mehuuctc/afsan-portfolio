409374691fdbdbd3943521a6e36c82e4c62f3e55
Mehzabeen Taher Zerin <260311028@uctc.edu.bd>
2026-08-20 02:44:25 +0600

    Remove AnalyticsMiddleware from settings
    Removed AnalyticsMiddleware from middleware settings.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.AnalyticsMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
